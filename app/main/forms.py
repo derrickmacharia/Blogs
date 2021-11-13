a0eafbc677bcbde6a1ccdfc99abd6d937ea7fade
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-11-13 16:30:24 +0300

    add blogs

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from wtforms import StringField,TextAreaField, SubmitField, SelectField
+from wtforms.validators import Required, Length
+from flask_wtf import FlaskForm
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+    blog_title = StringField('Blog title', validators=[Required()])
+    category = SelectField('Blog category',choices=[('Select a category','Select a category'),('Fashion', 'Fashion'),('Sports','Sports'),('Travel','Travel'),('Tech','Tech')], validators=[Required()])
+    content = TextAreaField('Body', validators=[Required()])
+    created_by= StringField('Blog author',validators=[Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
 from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
-from ..models import User
+from ..models import User,Quote,Blog
 from flask_login import login_required,current_user
-from .. import db
+from .. import db,photos
+from .forms import UpdateProfile
+from ..request import get_quote
+from .forms import BlogForm
 
 
 #views
@@ -11,4 +14,74 @@ def index():
     '''
     View root page function that returns the index page and its data.
     '''
-    return render_template('index.html')
+    quote = get_quote()
+    title = 'LetsBlog | Quotes'
+    return render_template('index.html', title = title,quote = quote)
+
+@main.route('/')
+def index():
+    '''
+    View root function that returns index template
+    '''
+
+    quote = get_quote()
+    blog_form = BlogForm()
+    all_blogs = Blog.query.order_by(Blog.date_posted).all()
+    return render_template('index.html',quote =quote,blogs = all_blogs)
+
+@main.route('/blog', methods=['GET', 'POST'])
+@login_required
+def new_blog():
+    blog_form = BlogForm()
+    if blog_form.validate_on_submit():
+        title = blog_form.blog_title.data
+        category = blog_form.category.data
+        content = blog_form.content.data
+        created_by = blog_form.content.data
+        new_blog = Blog(title=title,user=current_user,category=category, content=content, created_by= created_by)
+        new_blog.save_blog()
+        db.session.add(new_blog)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+
+    else:
+        all_blogs = Blog.query.order_by(Blog.date_posted).all()
+
+    return render_template('blogs.html', blogs=all_blogs,blog_form = blog_form)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    user = current_user._get_current_object().id
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile',uname=user.username))
+    return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+
